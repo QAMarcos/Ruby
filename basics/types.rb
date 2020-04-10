7b1fb6c7559de8f01081cab0a33c30dbce76fad5
Marcos <marcospereira.contact@gmail.com>
2020-04-10 02:17:27 -0300

    project settings and unit tests creating

---
+++ basics/types.rb
@@ -0,0 +1,23 @@
+#   Types Ruby:
+# The variables created have their types defined when they receive a certain value
+
+cellphone = 'Iphone X'
+price = 3000
+amount = 15
+available = true
+
+puts cellphone
+puts price
+puts amount
+puts available
+
+amount = '14' # the variable changes the type by assigning a "string" value.
+
+puts cellphone.class
+puts price.class
+puts amount.class
+puts available.class
+
+
+
+
